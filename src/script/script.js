b12b91bfab9f6709a800db30d47923353bc16514
Mahdi Mirchenari <mohammadmir9691@gmail.com>
2022-05-02 12:09:32 +0430

    update website

--- src/script/script.js
+++ src/script/script.js
@@ -1,7 +1,23 @@
+// nav bar
 const navIcon = document.querySelectorAll(".nav-icon");
 navIcon.forEach(i => {
     i.addEventListener("click", () => {
         navIcon.forEach(e => e.classList.remove("active"));
         i.classList.add("active");
+    })
+})
+
+// setting bar
+const setIcon = document.querySelector(".set-icon");
+const setDiv = document.querySelector(".page-setting");
+setIcon.addEventListener("click", () => {
+    setDiv.classList.toggle("active");
+})
+
+const colors = document.querySelectorAll(".color");
+const root = document.querySelector(":root");
+colors.forEach(i => {
+    i.addEventListener("click", () => {
+        root.classList = [i.classList[1]];
     })
 })
